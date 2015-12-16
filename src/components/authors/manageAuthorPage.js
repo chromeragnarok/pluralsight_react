3c09121926dd2cbddf3603e76630cdfe2649b882
Sidharta Surya Kusnanto <chromeragnarok@gmail.com>
2015-12-16 08:23:45 -0800

    added notification

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -2,8 +2,16 @@
 
 var React = require('react');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
+var history = require('history');
+var Router = require('react-router').Router;
+var History = require('react-router').History;
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
+    mixins: [
+        History
+    ],
     getInitialState: function(){
         return {
             author: { id: '', firstName: '', lastName: '' }
@@ -15,9 +23,17 @@ var ManageAuthorPage = React.createClass({
         this.state.author[field] = value;
         return this.setState({ author: this.state.author });
     },
+    saveAuthor: function(event){
+        event.preventDefault();
+        AuthorApi.saveAuthor(this.state.author);
+        toastr.success('Author saved!')
+        this.history.pushState(null, '/authors');
+    },
     render: function()  {
         return (
-            <AuthorForm author={this.state.author} onChange={this.setAuthorState}/>
+            <AuthorForm author={this.state.author}
+            onChange={this.setAuthorState}
+            onSave={this.saveAuthor} />
         )
     }
 });
